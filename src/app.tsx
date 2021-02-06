3bec84d461ae9e54dfb6933f583692361ce746a6
Ville Heikkinen <ville.heikkinen@gmail.com>
2021-02-06 14:24:06 +0200

    feat: Add react hello, prettier, tsconfigs

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const App = () => <h1>Hello</h1>
+
+
+
+const rootElem = document.getElementById('app')
+
+
+ReactDOM.render(<App/>, rootElem)
